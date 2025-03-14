eec8cdadf00325436c54c8d04c985305fc4afaf7
Zheng, Lei <realthunder.dev@gmail.com>
2025-03-14 21:17:10 +0800

    Update windows build script

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,9 @@
 @echo on
 
+:: free up extra disk space, compare
+:: https://github.com/conda-forge/conda-smithy/issues/1949
+rmdir /s /q C:\hostedtoolcache\windows
+
 if "%GIT_DESCRIBE_TAG:~-6%"=="stable" (
     set ICON=icon.ico
 ) else (
@@ -12,7 +16,7 @@ mkdir build
 cd build
 
 set "CFLAGS= "
-set "CXXFLAGS= "
+set "CXXFLAGS= -DBOOST_PROGRAM_OPTIONS_DYN_LINK=1"
 set "LDFLAGS_SHARED= ucrt.lib"
 
 REM remove Chocolatey/bin from search path to hide ccache, as it cause resouce compilation failure
@@ -36,31 +40,33 @@ cmake -G "Ninja" ^
       -D FREECAD_USE_OCC_VARIANT="Official Version" ^
       -D OCC_OCAF_LIBRARIES:FILEPATH=%LIBRARY_PREFIX%/lib ^
       -D BUILD_REVERSEENGINEERING:BOOL=ON ^
-      -D Python3_FIND_STRATEGY=LOCATION ^
-      -D PYTHON_EXECUTABLE:FILEPATH=%PREFIX%/python ^
-      -D Python3_EXECUTABLE:FILEPATH=%PREFIX%/python ^
       -D USE_BOOST_PYTHON:BOOL=OFF ^
       -D FREECAD_USE_PYBIND11:BOOL=ON ^
       -D SMESH_INCLUDE_DIR:FILEPATH=%LIBRARY_PREFIX%/include/smesh ^
       -D SMESH_LIBRARY:FILEPATH="%LIBRARY_PREFIX%/lib/SMESH.lib" ^
       -D FREECAD_USE_EXTERNAL_SMESH:BOOL=ON ^
+      -D FREECAD_USE_EXTERNAL_FMT:BOOL=OFF ^
       -D BUILD_FLAT_MESH:BOOL=ON ^
       -D BUILD_SHIP:BOOL=OFF ^
       -D BUILD_ASSEMBLY:BOOL=OFF ^
       -D OCCT_CMAKE_FALLBACK:BOOL=ON ^
+      -D Python_EXECUTABLE:FILEPATH="%PREFIX%/python" ^
+      -D PYTHON_EXECUTABLE:FILEPATH=%PREFIX%/python ^
+      -D Python3_EXECUTABLE:FILEPATH=%PREFIX%/python ^
       -D BUILD_DYNAMIC_LINK_PYTHON:BOOL=ON ^
       -D Boost_NO_BOOST_CMAKE:BOOL=ON ^
-      -D FREECAD_USE_CCACHE:BOOL=OFF ^
       -D FREECAD_USE_PCH:BOOL=OFF ^
+      -D FREECAD_USE_CCACHE:BOOL=OFF ^
       -D FREECAD_USE_PCL:BOOL=ON ^
       -D INSTALL_TO_SITEPACKAGES:BOOL=ON ^
-      -D LZMA_LIBRARY="%LIBRARY_PREFIX%/lib/liblzma.lib" ^
       -D BUILD_TEST:BOOL=OFF ^
+      -D ENABLE_DEVELOPER_TESTS:BOOL=OFF ^
       ..
+if %ERRORLEVEL% neq 0 exit 1
 
-if errorlevel 1 exit 1
 ninja install
-if errorlevel 1 exit 1
+
+if %ERRORLEVEL% neq 0 exit 1
 
 rmdir /s /q "%LIBRARY_PREFIX%\doc"
 REM  ren %LIBRARY_PREFIX%\bin\FreeCAD.exe freecad.exe
